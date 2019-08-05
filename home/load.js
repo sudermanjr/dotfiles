c00bb1f67b1e060ade6d1e9bde1bc67359c2f863
Andrew Suderman <andrew@sudermanjr.com>
2019-08-05 15:18:59 -0600

    Some small tweaks

--- home/load.js
+++ home/load.js
@@ -2,7 +2,7 @@ import http from "k6/http";
 import { check, fail } from "k6";
 
 export default function() {
-    let res1 = http.get(`http://${__ENV.TARGET}/`);
+    let res1 = http.get(`${__ENV.TARGET}`);
 
     check(res1, {
         "Status 200": (r) => r.status === 200
