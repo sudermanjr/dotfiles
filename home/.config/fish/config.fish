42cd23df13c820d55bd73a36714798777080d189
Andy Suderman <andy@suderman.dev>
2026-05-07 18:02:26 -0600

    fix: updates

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,6 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -gx FLOX_DISABLE_METRICS true
     if test -f /usr/local/bin/flox
-        eval "$(flox activate -s --dir ~)"
+        eval "$(flox activate --dir ~)"
     end
 end
